43eaf944cc0041996ccf9272038df0a3d0d4e6a9
Pierrick Bouvier <pierrick.bouvier@posteo.net>
2021-01-17 13:32:32 +0100

    iterate_array

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -8,12 +8,23 @@ fn compute(x: u32) -> u32
     res + 3
 }
 
+fn iterate_array()
+{
+    let a = [10, 20, 30, 40, 50];
+
+    for val in a.iter() {
+        println!("{}", val);
+    }
+}
+
 fn main() {
     let mut x = 3;
 
     let number = if compute(x) > 3 { 5 } else { 6 };
 
     x = number;
+
+    iterate_array();
 
     println!("x {}", x);
 }
